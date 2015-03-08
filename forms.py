5271b5a83bfa6cd0ce611e70af1689dd86ab0f4c
Maxim Tereshko <termax@pozitiff.ua>
2015-03-08 21:27:12 +0200

    some updates

---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+
+class CtrlForm(Form):
+    url = StringField('url', validators=[DataRequired()])
+    usr = StringField('usr', validators=[DataRequired()])
+    pwd = PasswordField('pwd', validators=[DataRequired()])
+    ver = StringField('ver', validators=[DataRequired()])
--- views.py
+++ views.py
@@ -1,9 +1,11 @@
 from flask import render_template, flash, redirect, url_for, request
 from terUNIFI import app, db
+from forms import CtrlForm
+from unifi.controller import Controller
 
 @app.route('/')
 def index():
-    return ('Hello World')
+    return render_template('index.html')
 
 @app.route('/test')
 def test():
@@ -15,4 +17,17 @@ def connect():
     for i in client:
         print ('{} arg: {}'.format(i, client[i]))
     return ('done')
+
+@app.route('/ctrl', methods=['GET', 'POST'])
+def ctrl():
+    form= CtrlForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        usr = form.usr.data
+        pwd = form.pwd.data
+        ver = form.ver.data
+        c = Controller(url, usr, pwd, ver)
+        return render_template('ctrl-view.html', ctrl_data = c)
+    return render_template('ctrl.html', form=form)
+
 
