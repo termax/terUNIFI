adc7e0ae69fb7f925d27cce42c8fab7ce09ce4d5
Maxim Tereshko <termax@pozitiff.ua>
2015-03-12 13:58:50 +0200

    +

--- views.py
+++ views.py
@@ -50,7 +50,7 @@ def portal():
     controller = WifiCtrl.query.get(ap.wifi_ctrl_id)
     return ("Device {}, is connected to AP: {} at Location: {} and \
             Controller: {} <br><br> {}"
-            .format(device, ap, location.name, controller.name, ctrl_args))
+            .format(device, ap.name, location.name, controller.name, ctrl_args))
 
 
 
